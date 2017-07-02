30a1c5ac60792a597281af6e44eae08f800020bf
Melika Norouzbeygi <melikanorouzbeygi75@gmail.com>
2017-07-02 23:03:58 +0430

    new message type added and improved

--- src/ir/aut/ceit/app/logic/NameMessage.java
+++ src/ir/aut/ceit/app/logic/NameMessage.java
@@ -1,22 +1,50 @@
 package ir.aut.ceit.app.logic;
 
-public class NameMessage extends TextMessage{
+import java.nio.ByteBuffer;
 
+public class NameMessage extends BaseMessage{
+    private String mName;
 
     public NameMessage(String name) {
-        super(name);
+        mName = name;
+        serialize();
     }
 
     public NameMessage(byte[] serialized) {
-        super(serialized);
+        mSerialized = serialized;
+        deserialize();
     }
 
     @Override
+    protected void serialize() {
+        int mNameLength = mName.getBytes().length;
+        int messageLength = 4 + 1 + 1 + 4 + mNameLength;
+        ByteBuffer byteBuffer = ByteBuffer.allocate(messageLength);
+        byteBuffer.putInt(messageLength);
+        byteBuffer.put(MessageTypes.PROTOCOL_VERSION);
+        byteBuffer.put(MessageTypes.NAME_MESSAGE);
+        byteBuffer.putInt(mNameLength);
+        byteBuffer.put(mName.getBytes());
+        mSerialized = byteBuffer.array();
+    }
+
+    protected void deserialize() {
+        ByteBuffer byteBuffer = ByteBuffer.wrap(mSerialized);
+        byteBuffer.getInt();
+        byteBuffer.get();
+        byteBuffer.get();
+        int mTextLength = byteBuffer.getInt();
+        byte[] mTextBytes = new byte[mTextLength];
+        byteBuffer.get(mTextBytes);
+        mName = new String(mTextBytes);
+    }
+
+
     public byte getMessageType() {
         return MessageTypes.NAME_MESSAGE;
     }
 
     public String getmName() {
-        return super.getText();
+        return mName;
     }
 }
