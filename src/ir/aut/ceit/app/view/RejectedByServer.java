3a29bafcfe065f158b49abd3dde722e87108bb0f
Melika Norouzbeygi <melikanorouzbeygi75@gmail.com>
2017-07-03 00:59:46 +0430

    some improvements on GUI

--- src/ir/aut/ceit/app/view/RejectedByServer.java
+++ src/ir/aut/ceit/app/view/RejectedByServer.java
@@ -7,23 +7,26 @@ import java.awt.event.ActionListener;
 
 public class RejectedByServer extends JFrame {
 
-    public RejectedByServer(){
+    private boolean ok;
+
+    public RejectedByServer() {
         super("Server Rejected...");
-        setLayout(new GridLayout(2,1));
+        setLayout(new GridLayout(2, 1));
         setSize(400, 200);
-        setLocation(700,400);
+        setLocation(700, 400);
         createWindow();
     }
 
-    public void createWindow(){
+    public void createWindow() {
         JLabel text = new JLabel("The server rejected your connection, click OK to exit");
 
         JPanel cancelPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
 
-        JButton cancel =  new JButton("OK");
+        JButton cancel = new JButton("OK");
         cancel.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                ok = true;
                 dispose();
             }
         });
@@ -33,4 +36,7 @@ public class RejectedByServer extends JFrame {
     }
 
 
+    public boolean isOk() {
+        return ok;
+    }
 }
